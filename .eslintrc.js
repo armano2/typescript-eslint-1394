ec6dbcd0231d377785dbae1c1e9b4c4bfad5b533
Armano <armano2@users.noreply.github.com>
2020-01-10 01:29:36 +0100

    simplify test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    parser: "vue-eslint-parser",
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        parser: "@typescript-eslint/parser",
-        project: "./tsconfig.json",
-		sourceType: "module",
-		extraFileExtensions: [".vue"]
-    },
-    rules: {
-        "@typescript-eslint/no-explicit-any": "error",
-    }
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+    sourceType: 'module',
+    extraFileExtensions: ['.vue']
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error'
+  }
 };
--- src/tmp.vue
+++ src/tmp.vue
@@ -1,14 +1,14 @@
 <template>
-    <div></div>
+  <div></div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { Component, PropSync } from 'vue-property-decorator'
+import Vue from 'vue';
+import { Component, PropSync } from 'vue-property-decorator';
 
 @Component
 export default class Tmp extends Vue {
-    @PropSync('test', { type: Array, required: true })
-    readonly foo: string | any
+  @PropSync('test', { type: Array, required: true })
+  readonly foo: string | any;
 }
 </script>
